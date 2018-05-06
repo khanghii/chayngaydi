6946e4d255e5a544ffba972efae3384732c83972
Duy .Ng <38210249+wy3@users.noreply.github.com>
2018-05-06 12:03:32 +0700

    upload

---
+++ assets/app.js
@@ -0,0 +1,8 @@
+function imgdblclick()
+{
+    var img = document.getElementsByName("img");
+    var oldurl = img.src;
+    var url = prompt("Please insert your image URL!");
+    if (!url) url = oldurl;
+    img.src = url;
+}
